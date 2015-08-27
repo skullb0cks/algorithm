98f426e9d90eece9832ed287d09086e36a4d9056
skullbocks <newday.uk@gmail.com>
2015-08-27 11:25:38 +0900

    mergeSortedArray

---
+++ src/mergeSortedArray.js
@@ -0,0 +1,37 @@
+function mergeArray(array1, array2) {
+    var res = array1;
+    for(var i=0, len=array2.length; i<len; i++) {
+        res.push(array2[i]);
+    }
+    return res
+}
+
+function getMinIndex(array) {
+    var min = null,
+        idx;
+    for(var i=0, len=array.length; i<len; i++) {
+        if(array[i] === null) continue;
+        if(min === null || min > array[i] || min == array[i]) {
+            min = array[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+function sortedArray(array) {
+    var res = [],
+        k = array.length;
+    while(k > 0) {
+        var idx = getMinIndex(array),
+            min = array[idx];
+        res.push(min);
+        array[idx] = null;
+        --k;
+    }
+    return res;
+}
+
+function mergeSortedArray(array1, array2) {
+    return sortedArray(mergeArray(array1, array2));
+}
---
+++ test/mergeSortedArray.spec.js
@@ -0,0 +1,21 @@
+describe('algorithm', function() {
+    it('mergeArray', function() {
+        var result = mergeArray([2,5,6,9], [1,2,3,29]);
+        expect(result).toEqual(jasmine.arrayContaining([2, 5, 6, 9, 1, 2, 3, 29]));
+    });
+
+    it('sortedArray', function() {
+        var result = sortedArray([2, 51, 6, 9, 1, 21, 3, 29]);
+        expect(result).toEqual(jasmine.arrayContaining([1, 2, 3, 6, 9, 21, 29, 51]));
+    });
+
+    it('getMinIndex', function() {
+        var result = getMinIndex([null, 1, null, 9, null, 21, 3, 29]);
+        expect(result).toBe(1);
+    });
+
+    it('mergeSortedArray', function() {
+        var result = mergeSortedArray([2,5,6,9], [1,2,3,29]);
+        expect(result).toEqual(jasmine.arrayContaining([1, 2, 2, 3, 5, 6, 9, 29]));
+    });
+});
